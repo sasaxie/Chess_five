a799925028ed9be18f1f7c3ecaa26ee4c2d797d6
qianfeng <qianfeng@qianfengdeMacBook-Pro-9.local>
2015-08-04 18:00:24 +0800

    Initial Commit

---
+++ Chess_five/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Chess_five
+//
+//  Created by qianfeng on 15-8-4.
+//  Copyright (c) 2015年 yanda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Chess_five/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Chess_five
+//
+//  Created by qianfeng on 15-8-4.
+//  Copyright (c) 2015年 yanda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
